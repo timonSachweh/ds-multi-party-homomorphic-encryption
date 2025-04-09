ec282f3662e4a6d6bcdbc6296e5b04ad8c8e000e
Timon Sachweh <timon@sachweh.de>
2025-04-09 17:24:16 +0200

    Adding python test client to simulate a smaller weights vector

---
+++ python-client/src/test_client_main.py
@@ -0,0 +1,49 @@
+import json
+
+import config
+from flask import Flask
+import logging
+from flask import request
+import numpy as np
+from utils import disable_logging
+
+app = Flask(__name__)
+app.logger.setLevel(logging.ERROR)
+c = config.Config()
+
+weights = np.full(20, 3.14513, dtype=np.float32)
+
+@app.route('/health')
+@disable_logging
+def health_route():
+    return {
+        'message': 'Service is ready'
+    }
+
+@app.route(c.server.api + '/train')
+def train_route():
+    return {
+        'message': 'Training complete!'
+    }
+
+@app.route(c.server.api + '/model-params', methods=['GET', 'POST'])
+def model_params_route():
+    if request.method == 'GET':
+        response = json.dumps({
+            "model_name": c.model.name,
+            "version": c.model.version,
+            "weights": weights
+        })
+        return response
+    elif request.method == 'POST':
+        body = request.get_json()
+        print("original weights:")
+        print(weights)
+        print("new weights:")
+        print(np.array(body['weights'], dtype=np.float32))
+        return {
+            'message': 'Model parameters updated!'
+        }
+
+if __name__ == '__main__':
+    app.run(debug=c.server.debug, host="0.0.0.0", port=c.server.port)
