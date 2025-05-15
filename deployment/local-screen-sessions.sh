7841bb07293aa512dfd4ad1e82c79b6354ac65b6
Timon Sachweh <timon@sachweh.de>
2025-05-15 15:49:57 +0200

    Change deployment skript to allow dynamic amount of clients

--- deployment/local-screen-sessions.sh
+++ deployment/local-screen-sessions.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
-main_path="$(dirname "$0")/../"
+BASE_PATH="$(dirname "$(readlink -f "$0")")/.."
 
 # Function to start services
 run_services() {
     echo "Starting services..."
 
     # Start a screen session for each service
-    screen -dmS "he-aggregation-service" bash -c "source '${main_path}/env/server.env'; go run ${main_path}aggregation-server/cmd/main.go"
+    screen -dmS "he-aggregation-service" bash -c "\
+        source '${BASE_PATH}/env/server.env'; \
+        go run ${BASE_PATH}/aggregation-server/cmd/main.go"
 
-    screen -dmS "he-client-1-go" bash -c "
-    source '${main_path}/env/client1.env'; go run ${main_path}he-client/cmd/main.go"
-    screen -dmS "he-client-1-python" bash -c "source '${main_path}/env/client1.env'; pyenv activate ml; python ${main_path}python-client/src/main.py"
-
-    screen -dmS "he-client-2-go" bash -c "
-    source '${main_path}/env/client2.env'; go run ${main_path}he-client/cmd/main.go"
-    screen -dmS "he-client-2-python" bash -c "source '${main_path}/env/client2.env'; pyenv activate ml; python ${main_path}python-client/src/main.py"
-
+    sleep 2
+    for (( i=1; i<=$1; i++ ))
+    do
+        GO_PORT=$((8080+i))
+        PYTHON_PORT=$((9090+i))
+        PARTY_IDX=$((i-1))
+        echo "Starting client $i with ports: ${GO_PORT} and ${PYTHON_PORT}..."
+        screen -dmS "he-client-${i}-go" bash -c "\
+            source '${BASE_PATH}/env/client1.env'; \
+            export HTTP_PORT=${GO_PORT}; \
+            export EXTERNAL_URL=http://localhost:${GO_PORT}; \
+            export PYTHON_PORT=${PYTHON_PORT}; \
+            export DATA_SPLIT_PARTY=${PARTY_IDX}; \
+            export DATA_SPLIT_NUM_PARTIES=$1; \
+            go run ${BASE_PATH}/he-client/cmd/main.go"
+        screen -dmS "he-client-${i}-python" bash -c "\
+            source '${BASE_PATH}/env/client1.env'; \
+            export HTTP_PORT=${GO_PORT}; \
+            export EXTERNAL_URL=http://localhost:${GO_PORT}; \
+            export PYTHON_PORT=${PYTHON_PORT}; \
+            export DATA_SPLIT_PARTY=${PARTY_IDX}; \
+            export DATA_SPLIT_NUM_PARTIES=$1; \
+            pyenv activate ml; \
+            python ${BASE_PATH}/python-client/src/main.py"
+        sleep 8
+    done
     
     sleep 2
     
@@ -42,7 +62,11 @@ stop_services() {
 
 # Check the first parameter
 if [ "$1" = "run" ]; then
-    run_services
+    NUM_CLIENTS=2
+    if [ -n "$2" ]; then
+        NUM_CLIENTS=$2
+    fi
+    run_services $NUM_CLIENTS
 elif [ "$1" = "down" ]; then
     stop_services
 else
