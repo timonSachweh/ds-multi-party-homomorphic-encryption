c184d9e78988e810d12bdacd44d857594a00e1a6
Timon Sachweh <timon@sachweh.de>
2025-08-04 16:44:42 +0200

    adding executable for running python service standalone

---
+++ deployment/gobal-python-run.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+BASE_PATH="$(dirname "$(readlink -f "$0")")/.."
+
+
+
+screen -L -Logfile "${BASE_PATH}/logs/global-python.log" -dmS "global-python" zsh -c "\
+            source '${BASE_PATH}/env/client1.env'; \
+            export PYTHON_PORT=5001; \
+            export DATA_SPLIT_PARTY=0; \
+            export DATA_SPLIT_NUM_PARTIES=1; \
+            pyenv activate ml; \
+            python ${BASE_PATH}/python-client/src/main.py"
