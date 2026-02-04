a123ea98a6341bd993649be0b0ba008310b9f283
Timon Sachweh <timon@sachweh.de>
2026-02-04 09:23:42 +0000

    Fix setting the url

--- deployment/kube-deployment.sh
+++ deployment/kube-deployment.sh
@@ -1,35 +1,38 @@
-#!/bin/zsh
+#!/bin/bash
 
 BASE_PATH="$(dirname "$(readlink -f "$0")")/.."
 
-alias skube="microk8s kubectl"
-alias shelm="microk8s helm"
+skube="microk8s kubectl"
+shelm="microk8s helm"
+
+prefix="ds"
 
 # Function to start services
 run_services() {
     echo "Starting services..."
 
     # Start a screen session for each service
-    shelm install ds ${BASE_PATH}/deployment/helm-charts/ds --set numClients=$1
+    $shelm install $prefix ${BASE_PATH}/deployment/aggregation-server --set numClients=$1
 
     sleep 2
     for (( i=1; i<=$1; i++ ))
     do
-        shelm install ds-c${i} ${BASE_PATH}/deployment/helm-charts/he-client --set clientIndex=$((i-1)) --set totalClients=$1
+        $shelm install $prefix-c${i} ${BASE_PATH}/deployment/he-client --set clientIndex=$((i-1)) --set totalClients=$1 --set service.aggregationService=http://$prefix-aggregation-server:8080/v1
         sleep 8
     done
-    
+
     sleep 2
-    
+
+    sessions=$shelm ls
     echo "Running sessions:"
-    echo "${shelm ls}"
+    echo "${sessions}"
 }
 
 # Function to stop services
 stop_services() {
     echo "Stopping services..."
-    shelm delete $(shelm ls -q | grep ds)
-    
+    $shelm delete $($shelm ls -q | grep ds)
+
     echo "All services have been stopped."
 }
 
